ed30347733e533df7e78dd87f13afd3c5396efd1
Zaggralez <jacob@pjetursson.com>
2017-09-20 11:40:01 +0200

    d

--- Project 1/Project1.py
+++ Project 1/Project1.py
@@ -8,10 +8,9 @@ Created on Tue Sep 19 16:20:42 2017
 
 from numpy import genfromtxt
 
-movieData = genfromtxt('movie_metadata.csv', delimiter=',')
+movieData = genfromtxt('movie_metadata.csv', delimiter=',',dtype=None, usecols=(1,2,3,8,9,11,12,16,18,19,20,21,22,23,25,26))
 #with open('movie_metadata.csv', newline='') as csvfile:
  #   movieData = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONE)
 for row in movieData:
     print(row)
-
         
