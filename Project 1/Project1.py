69aced71b37c16b712d21ee292b8504b76cf5fc0
Zaggralez <jacob@pjetursson.com>
2017-11-05 16:11:10 +0100

    Project 2 init

--- Project 1/Project1.py
+++ Project 1/Project1.py
@@ -6,51 +6,52 @@ Created on Tue Sep 19 16:20:42 2017
 @author: Jacob
 """
 import numpy as np
-import pandas as pd
-from matplotlib.pyplot import boxplot, xticks, figure, subplot,title, hist, xlabel, ylim, show
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA # Principal Component Analysis module
-from sklearn.cluster import KMeans # KMeans clustering 
+import dataSetup
+from matplotlib.pyplot import boxplot, plot, hold, xticks, figure, subplot,title, hist, xlabel, ylim, show
 import matplotlib.pyplot as plt # Python defacto plotting library
 import seaborn as sns # More snazzy plotting library
-from scipy.stats import zscore
 
-fields= ['budget','gross','genres', 'imdb_score', 'num_voted_users', 'movie_title']
-numberFields = ['budget','gross', 'imdb_score', 'num_voted_users']
-movie = pd.read_csv('movie.csv', encoding="latin-1",usecols=fields)
-numbersData = pd.read_csv('movie.csv', encoding="latin-1",usecols=numberFields)
-numbersData = numbersData.fillna(value=0, axis=1) # TODO - Wrong to swap with 0
-X = numbersData.values
-# Data Normalization
-X_std = StandardScaler().fit_transform(X)
-attributeNames = numbersData.columns.values
-M = len(attributeNames)
+numbersData = dataSetup.numbersData
+X = dataSetup.X
+M = dataSetup.M
+attributeNames = dataSetup.attributeNames
+# BOXPLOT               
+figure(figsize=(12,7))
+title('Movies: Boxplots')
+u = np.floor(np.sqrt(M)); v = np.ceil(float(M)/u)
+for i in range(M):
+    subplot(u,v,i+1)
+    boxplot(X[:,i], sym='k')
+    xlabel(attributeNames[i])
 
-# CORRELATION:
-# Set up the matplotlib figure
+#BOXPLOT BUDGET
+figure(figsize=(6,4))
+boxplot(X[:, 3], sym='k')
+xlabel("Budget")
+
+figure(figsize=(6,4))
+boxplot(X[:, 0], sym='k')
+xlabel("Duration")
+#xticks(range(1,M+1), attributeNames, rotation=45)
+
+# HISTOGRAMS OF ALL ATTRIBUTES
+figure(figsize=(12,7))
+u = np.floor(np.sqrt(M)); v = np.ceil(float(M)/u)
+for i in range(M):
+    subplot(u,v,i+1)
+    hist(X[:,i])
+    xlabel(attributeNames[i])
+    
+# SCATTER PLOT MATRIX
+# Set style of scatterplot
+sns.set_context("notebook", font_scale=1.1)
+sns.set_style("ticks")
+# Create scatterplot of dataframe
+sns.set(style="ticks")
+#pd.plotting.scatter_matrix(numbersData, alpha=0.2, figsize=(12,12), diagonal='kde')
+sns.pairplot(numbersData, size=1.5)
+
+# CORRELATION MATRIX:
 f, ax = plt.subplots(figsize=(10, 8))
-plt.title('Correlation of movie data')
+plt.title('Pearson Correlation of movie data')
 sns.heatmap(numbersData.astype(float).corr(),linewidths=0.2,vmax=1.0, square=True, cmap="YlGnBu", linecolor='black', annot=True)
-
-
-# OUTLIERS
-# TITLES OF OUTLIERS
-for i in movie:
-    for k in movie.T:
-        if(i == 'budget'):
-            if(movie[i][k] > 500000000):
-                print(movie['movie_title'][k])
-                
-figure(figsize=(12,6))
-title('Movies: Boxplots')
-boxplot(X, sym='k')
-xticks(range(1,M+1), attributeNames, rotation=45)
-
-# OUTLIERS REMOVED
-outlier_mask = (X[:,2]>500000000)
-valid_mask = np.logical_not(outlier_mask)
-X = X[valid_mask,:]
-figure(figsize=(12,6))
-title('Movies: Boxplots')
-boxplot(X, sym='k')
-xticks(range(1,M+1), attributeNames, rotation=45)
