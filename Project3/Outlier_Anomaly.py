ffdebf1a6a2a1a89b629eddba1562ce739c4b07f
Zaggralez <jacob@pjetursson.com>
2017-11-27 22:00:26 +0100

    mm

--- Project3/Outlier_Anomaly.py
+++ Project3/Outlier_Anomaly.py
@@ -36,7 +36,11 @@ K = 200
 
 # x-values to evaluate the KNN
 xe = np.linspace(-10, 10, 100)
+
 x = np.linspace(-10, 10, 50)
+
+
+# RANDOM DATASET
 N = 1000; M = 2
 X = np.empty((N,M))
 m = np.array([1, 3, 6]); s = np.array([1, .5, 2])
@@ -44,19 +48,25 @@ c_sizes = np.random.multinomial(N, [1./3, 1./3, 1./3])
 for c_id, c_size in enumerate(c_sizes):
     X[c_sizes.cumsum()[c_id]-c_sizes[c_id]:c_sizes.cumsum()[c_id],:] = np.random.normal(m[c_id], np.sqrt(s[c_id]), (c_size,M))
 
+
 # Find the k nearest neighbors
 knn = NearestNeighbors(n_neighbors=K).fit(X)
-D, i = knn.kneighbors(np.matrix(xe).T)
+print(knn.kneighbors(np.matrix(xe).T))
+#D, i = knn.kneighbors(np.matrix(xe).T)
 
+"""
 # Compute the density
 #D, i = knclassifier.kneighbors(np.matrix(xe).T)
 knn_density = 1./(D.sum(axis=1)/K)
+"""
 
+"""
 # Compute the average relative density
 DX, iX = knn.kneighbors(X)
 knn_densityX = 1./(DX[:,1:].sum(axis=1)/K)
 knn_avg_rel_density = knn_density/(knn_densityX[i[:,1:]].sum(axis=1)/K)
 
+"""
 # Plot KNN density
 figure(figsize=(6,7))
 subplot(2,1,1)
