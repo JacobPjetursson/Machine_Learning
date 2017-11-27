af90d5ddf62fbb2b853d25cfb56ffcd65fb25652
Thomas <s153047@student.dtu.dk>
2017-11-27 21:49:36 +0100

    fixede det så det ligner det gamle

--- Project 1/Project1.py
+++ Project 1/Project1.py
@@ -10,24 +10,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from dataSetup import *
 
-fields= ['budget','gross','genres', 'imdb_score', 'num_voted_users']
-numberFields = ['budget','gross', 'imdb_score', 'num_voted_users']
-movie = pd.read_csv('movie.csv', encoding="latin-1",usecols=fields)
-numbersData = pd.read_csv('movie.csv', encoding="latin-1",usecols=numberFields)
-numbersData = numbersData.fillna(value=0, axis=1) # TODO - Wrong to swap with 0
-
-
-X = numbersData.values
-# Data Normalization
-X_std = StandardScaler().fit_transform(X)
 
 pca = PCA(n_components=4)
 x_4d = pca.fit_transform(X_std)
 
 plt.figure(figsize=(10,7))
 plt.scatter(x_4d[:,0],x_4d[:,1], c='goldenrod', alpha=0.5)
-plt.ylim(-10,30)
+plt.ylim(-5,7)
 plt.show
 
 #print(X_std)
